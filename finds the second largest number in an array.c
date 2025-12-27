fd86f40b59e4b97297bb4c3a158dc4b3480b63b7
joydeep012 <joydeep01234@gmail.com>
2025-12-27 22:24:47 +0530

    Implement program to find second largest number

---
+++ finds the second largest number in an array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int n;
+    printf("enter the size of the array : ");
+    scanf("%d",&n);
+    int a[n];
+    if(n<2){
+        printf("Minimum requirement for array size is 2.\n");
+        return 1;
+    }
+    else{
+        for(int i=0;i<n;i++){
+            printf("%d enter the no for the array :",i+1);
+            scanf("%d",&a[i]);
+            }
+int l=INT_MIN,s=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(l<a[i]){
+                s=l;
+                l=a[i];}
+            else if(a[i]>s&&a[i]!=l){
+                s=a[i];
+            }
+            
+    }
+    if(s == INT_MIN){
+        printf("All elements are the same.\n");
+    } else {
+        printf("The second largest number is: %d\n", s);
+    }
+}return 0;}
