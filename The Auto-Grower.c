8b2ad88315fdf8583fea72e98da786ba25155cd7
joydeep012 <joydeep01234@gmail.com>
2026-01-26 00:14:37 +0530

    Add linked list implementation in The Auto-Grower.c

---
+++ The Auto-Grower.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+void insertAtEnd(struct Node* head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+int main() {
+    int a;
+    printf("enter a no : ");
+    scanf("%d",&a);
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head->data = 100;
+    head->next = NULL;
+    insertAtEnd(head, a);
+    printList(head);
+    return 0;
+}
